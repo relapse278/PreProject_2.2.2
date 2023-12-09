f921c9ba5e0e70496e07476d8372034da4322a08
Руслан Шевченко <r.schewtschenko@mail.ru>
2023-12-09 19:43:19 +0700

    Added the calc HTML site as an example.

---
+++ src/main/java/web/controller/CalculatorController.java
@@ -0,0 +1,33 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CalculatorController {
+    @GetMapping(value = "/calc")
+    public String calculate(@RequestParam(value = "operand1", required = false) int operand1,
+                            @RequestParam(value = "operand2", required = false) int operand2,
+                            @RequestParam(value = "operator", required = false) String operator,
+                            Model model) {
+        double result = 0.0;
+        switch (operator) {
+            case "+":
+                result = operand1 + operand2;
+                break;
+            case "-":
+                result = operand1 - operand2;
+                break;
+            case "*":
+                result = operand1 * operand2;
+                break;
+            case "/":
+                result = operand1 / operand2;
+                break;
+        }
+        model.addAttribute("result", result);
+        return "calc";
+    }
+}
